47301a3d4e5a57be516c42794a63da00487fa77c
SharangP <sharang.phadke@gmail.com>
2015-05-14 14:55:43 +0000

    vim is done

--- vim/get_bundles.sh
+++ vim/get_bundles.sh
@@ -4,4 +4,5 @@ git clone https://github.com/scrooloose/nerdcommenter.git
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/scrooloose/syntastic.git
+git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/bling/vim-airline.git
--- vimrc
+++ vimrc
@@ -88,3 +88,5 @@ let g:syntastic_check_on_wq = 0
 "
 set laststatus=2
 let g:airline_powerline_fonts = 1
+let g:airline#extensions#tabline#enabled = 1
+let g:airline_theme='dark'
