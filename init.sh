938dfecf7b0cf2497c3c51ccff782684d8e13453
SharangP <sharang.phadke@gmail.com>
2015-05-15 11:01:18 +0000

    added packages, TODO, redshift, xinitrc

--- init.sh
+++ init.sh
@@ -3,11 +3,13 @@ CONFIGPATH=`dirname $CONFIGPATH`
 ln -s "$CONFIGPATH/i3" ~/.i3
 ln -s "$CONFIGPATH/Xdefaults" ~/.Xdefaults
 ln -s "$CONFIGPATH/Xresources" ~/.Xresources
+ln -s "$CONFIGPATH/xinitrc" ~/.xinitrc
 ln -s "$CONFIGPATH/bashrc" ~/.bashrc
 ln -s "$CONFIGPATH/zshrc" ~/.zshrc
 ln -s "$CONFIGPATH/vim" ~/.vim
 ln -s "$CONFIGPATH/vimrc" ~/.vimrc
 ln -s "$CONFIGPATH/gitconfig" ~/.gitconfig
+ln -s "$CONFIGPATH/redshift" ~/.config/redshift.conf
 
 #install fonts
 git clone https://github.com/powerline/fonts.git
---
+++ packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install pacman packages
+sudo pacman -S zsh, vim, vlc, rxvt-unicode, yaourt, mount, htop, firefox, ttf-dejavu, python-pip, redshift, the_silver_searcher
+
+# install yaourt packages
+yaourt -S light
