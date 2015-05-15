952d0cf1de93d68bcbb6f17358d9abd8e64de827
SharangP <sharang.phadke@gmail.com>
2015-05-15 19:18:56 +0000

    added flash

--- packages.sh
+++ packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install pacman packages
-sudo pacman -S zsh, vim, vlc, rxvt-unicode, yaourt, mount, htop, firefox, ttf-dejavu, python-pip, redshift, the_silver_searcher
+sudo pacman -S zsh vim vlc rxvt-unicode yaourt mount htop firefox ttf-dejavu python-pip redshift the_silver_searcher chromium-pepper-flash
 
 # install yaourt packages
 yaourt -S light
